fab626264f3a5f300a343fab671a741d25f317a9
Parham Fox <parhamfh@kth.se>
2012-05-24 00:00:43 +0200

    Added major drawing functionality.
    Now you can draw stuff and change the color of the background and lines. It
    works. Is robust. Hopefully.

--- muscovaudio.py
+++ muscovaudio.py
@@ -14,20 +14,14 @@ class Colour(object):
 	AQUAMARINE = (127 ,255, 212)
 	CHOCOLATE = (255, 127, 36)
 	TURQUOISE = (0, 245, 255)
+	GOLFGREEN = (0,100,27)
 	
 class Muscovaudio(object):
 	def __init__(self):
-		print "running"
-		self.canvas_width =630
-		self.canvas_height = 470
-		self.run()
-	
-	def draw(self):
-		
-		self.canvas.draw_content()
-		self.window.blit(self.canvas, (5,5))
-	
+		print "init"
+			
 	def run(self):
+		print "running"
 		# Initialize pygame
 		pygame.init()
 			
@@ -35,17 +29,23 @@ class Muscovaudio(object):
 		pygame.display.set_caption('MUSCOVAUDIO')
 		
 		# Create canvas
-		self.canvas = Canvas(self.canvas_width, self.canvas_height, Colour.BLACK)
+		self.canvas = Canvas()
+		
+		# Create mouse canvas
 		
 		# Create & init application Window 
 		self.window = Window()
 		self.window.init_window()
 		
-		# Init MouseHandler against our canvas
-		mh = MouseHandler(self.canvas)
+		# Add our canvas to the window
+		self.window.add_canvas(self.canvas)
+		
+		# Init MouseHandler against our mouse
+		self.mouse = Mouse()
+		mh = MouseHandler(self.mouse, self.canvas)
 		
 		# Update the pygame display
-		self.draw()
+		self.window.draw()
 		pygame.display.update()		
 		
 		while True:
@@ -59,34 +59,39 @@ class Muscovaudio(object):
 						if e.key == K_ESCAPE:
 							pygame.quit()
 							sys.exit(0)
-							
 				# Update the pygame display
-				self.draw()
+				self.window.draw()
 				pygame.display.update()
 				
 class Canvas(Surface):
 	
-	def __init__(self, width, height, color):
+	def __init__(self, width = 630, height = 470, color = Colour.GOLFGREEN):
 		super(Canvas, self).__init__((width, height))
+		
+		# Canvas color
+		self.color = color
 		
 		# Canvas for temporary lines
 		self.work_canvas = Surface((width, height)) 
-		self.color = color
 		self.work_canvas.set_colorkey(color)
 		
 		# Canvas for drawn lines 
 		self.line_canvas = Surface((width, height))
+		self.line_canvas.fill(self.color)
+
 
 		# List of lines
 		self.lines = []
 		
-		# Mouse info text position
+		# Mouse info text position on Canvas
 		self.mouse_info_x = 0
 		self.mouse_info_y = 0
-		self.mouse_canvas = Surface ((200,100))
+		self.mouse_info_canvas = Surface ((200,100))
+		self.mouse_info_canvas.set_colorkey(color)
 		
-		# Mouse canvas
-		self.mouse_canvas.set_colorkey(color)
+		# Fill the auxilliary canvases so that they become transparent
+		self.work_canvas.fill(self.color)
+		self.mouse_info_canvas.fill(self.color)
 		
 	def draw_measuring_line(self,start,end):
 		# Erase old measuring line
@@ -97,27 +102,28 @@ class Canvas(Surface):
 	def draw_line(self,start,end):
 		# Draw an actual, permanent line and erase measuring line
 		self.work_canvas.fill(self.color)
-		pygame.draw.line(self.line_canvas, Colour.TURQUOISE, start, end,5)
+#		self.mouse_info_canvas.fill(self.color)
+		pygame.draw.line(self.line_canvas, Colour.TURQUOISE, start, end, 5)
 	
-	def draw_mouse_info(self, message, dragging=False):
+	def draw_mouse_info(self, message, drawing=False):
 		# Empty mouse canvas
-		self.mouse_canvas.fill(self.color)
+#		self.mouse_info_canvas.fill(self.color)
 		# Create font and blit onto canvas
 		font = Font(None, 22)
 		mouse_info = font.render(message, 1, Colour.WHITE, Colour.BLUE)
-		self.mouse_canvas.blit(mouse_info, (self.mouse_info_x, self.mouse_info_y))
+		self.mouse_info_canvas.blit(mouse_info, (self.mouse_info_x, self.mouse_info_y))
 		
 		# If Mouse is dragging, notify
-		if dragging:
-			ti = Font(None, 30).render("DRAGGING",1,Colour.CHOCOLATE, Colour.WHITE)
-			self.mouse_canvas.blit(ti, (self.mouse_info_x, self.mouse_info_y+20))
+		if drawing:
+			ti = Font(None, 30).render("DRAWING",1,Colour.CHOCOLATE, Colour.WHITE)
+			self.mouse_info_canvas.blit(ti, (self.mouse_info_x, self.mouse_info_y+20))
 	
 	def draw_content(self):
 		# Draw the stuff on the canvas 
 		self.blit(self.line_canvas,(0,0))
 		self.blit(self.work_canvas,(0,0))
-		self.blit(self.mouse_canvas,(self.get_width()-240,self.get_height()-80))
-	
+		self.blit(self.mouse_info_canvas,(self.get_width()-240,self.get_height()-80))
+		
 class Window(object):
 	
 	def __init__(self, width=640, height=480, bkg_color=Colour.WHITE):
@@ -126,31 +132,35 @@ class Window(object):
 		self.background_color = bkg_color
 		self.window = None
 		self.init_window()
+		self.canvas = None
+		
+	def add_canvas(self, canvas, canvas_x=5, canvas_y=5):
+		self.canvas = canvas
+		self.canvas_x = canvas_x
+		self.canvas_y = canvas_y
+		
+	def draw(self):
+		self.canvas.draw_content()
+		self.window.blit(self.canvas, (self.canvas_x,self.canvas_y))
 		
 	def init_window(self):
 		self.window = pygame.display.set_mode((self.width,self.height))
 		self.window.fill(self.background_color)
 		
-	def change_background_color(self, r_g_b):
-		self.window.fill(r_g_b)
+	def change_background_color(self, color):
+		self.window.fill(color)
 	
 	def blit(self, canvas, pos):
 		self.window.blit(canvas, pos)
 
 class MouseHandler(object):
 	
-	BUTTONS = {1:"Left Button", 3:"Right Button"}
-
-	def __init__(self, canvas):
+	def __init__(self, mouse, canvas):
 		
-		self.canvas = canvas
-		
-		# Mouse states
-		self.mouse_down = False
-		self.mouse_button = 0
+		self.canvas = canvas	
+		self.mouse = mouse
 		
 		# Mouse action 
-		self.dragging = False
 		self.drag_start_x = 0
 		self.drag_start_y = 0
 		self.drag_end_x = 0
@@ -158,56 +168,81 @@ class MouseHandler(object):
 		
 	def handle_event(self, event):
 		# Get mouse position
-		(self.x, self.y) = pygame.mouse.get_pos()
+		(self.mouse_x, self.mouse_y) = pygame.mouse.get_pos()
 		
 		# Check if the event is mouse-related
 		if event.type == pygame.MOUSEBUTTONDOWN:
-			self.mouse_down = True
-			self.mouse_button = self.BUTTONS[event.button]  
+			self.mouse.mouse_pressed()
+			self.mouse.set_button_pressed(event.button)  
 				
 		elif event.type == pygame.MOUSEBUTTONUP:
-			self.mouse_down = False
-			self.mouse_button = self.BUTTONS[event.button]
+			self.mouse.mouse_released()
+			self.mouse.set_button_pressed(event.button)  
 		
 		self.measure_line()
 		# Write out mouse info
 		self.write_mouse_info()
 	
-	@property
-	def left_button_pressed(self):
-#		print self.mouse_button == "Left Button"
-		return self.mouse_button == "Left Button"
-	
 	def measure_line(self):
-		if not self.dragging and self.mouse_down and self.left_button_pressed:
-			self.drag_start_x = self.x
-			self.drag_start_y = self.y
-			self.drag_end_x = self.x
-			self.drag_end_y = self.y
-			self.dragging = True
+		if not self.mouse.dragging and self.mouse.mouse_down and self.mouse.left_button_pressed:
+			self.drag_start_x = self.mouse_x
+			self.drag_start_y = self.mouse_y
+			self.drag_end_x = self.mouse_x
+			self.drag_end_y = self.mouse_y
+			self.mouse.set_dragging(True)
 			
-		elif self.dragging and self.mouse_down and self.left_button_pressed:
-			self.drag_end_x = self.x
-			self.drag_end_y = self.y 
+		elif self.mouse.dragging and self.mouse.mouse_down and self.mouse.left_button_pressed:
+			self.drag_end_x = self.mouse_x
+			self.drag_end_y = self.mouse_y 
 			self.canvas.draw_measuring_line((self.drag_start_x,self.drag_start_y),(self.drag_end_x, self.drag_end_y))
-		
-		elif self.dragging and not self.mouse_down and self.left_button_pressed:
-			self.dragging = False
-			self.canvas.draw_line((self.drag_start_x,self.drag_start_y),(self.drag_end_x, self.drag_end_y))
 			
+		elif self.mouse.dragging and not self.mouse.mouse_down and self.mouse.left_button_pressed:
+			self.mouse.set_dragging(False)
+			self.canvas.draw_line((self.drag_start_x,self.drag_start_y),(self.drag_end_x, self.drag_end_y))
+
 	def write_mouse_info(self):
 		
 		# Build string
 		message = "x = %s "\
-				  "y = %s"%(self.x, self.y)
+				  "y = %s"%(self.mouse_x, self.mouse_y)
 		
 		# If key pressed add that info
-		if self.mouse_down:
-			message = message + " %s"%self.mouse_button
+		if self.mouse.mouse_down:
+			message = message + " %s"%self.mouse.button_pressed
 
 		# Draw Mouse info on canvas
-		self.canvas.draw_mouse_info(message,self.dragging)		
+		self.canvas.draw_mouse_info(message,self.mouse.dragging)		
 
+	
+class Mouse(object):
+	
+	BUTTONS = {1:"Left Button", 3:"Right Button"}
+	
+	def __init__(self):
+		self.mouse_down = False
+		self.button_pressed = "None"
+		self.swiping = False
+		
+	def mouse_pressed(self):
+		self.mouse_down = True
+	
+	def mouse_released(self):
+		self.mouse_down = False
+	
+	@property
+	def dragging(self):
+		return self.swiping
+		
+	def set_dragging(self, sw):
+		self.swiping = sw
+		
+	@property
+	def left_button_pressed(self):
+		return self.button_pressed == "Left Button"
+	
+	def set_button_pressed(self, button):
+		self.button_pressed = self.BUTTONS[button]
+		
 class Ball(object):
 
 	def __init__(self, x_init, y_init, r, vel=0):
@@ -216,7 +251,7 @@ class Ball(object):
 		self.radius = r
 		self.velocity = vel
 
-
 if __name__ == "__main__":
 	muscovaudio = Muscovaudio()
+	muscovaudio.run()
 	
